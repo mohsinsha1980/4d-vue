7ffae700694ecd967540d6d4ebc0977989e5c7b9
Mohsin Shaikh <mohsinsha@gmail.com>
2022-09-15 20:08:11 +0530

    Initial commit

---
+++ vue/src/components/FourDComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>{{ remotemsg }}</h2>
+  </div>
+</template>
+
+<script>
+const https = require("http");
+export default {
+  name: "4DComponent",
+  props: {
+    msg: String,
+    remotemsg: String,
+  },
+  methods: {
+    getRemoteMsg() {
+      var url = window.location.href + "hello";
+      https.get(url, (res) => {
+        res.on("data", (data) => {
+          this.remotemsg = data;
+        });
+      });
+    },
+  },
+  created() {
+    this.getRemoteMsg();
+  },
+};
+</script>
